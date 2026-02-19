c6e0f8270062bc106755a02c824bb2590519f6af
Debo Odulana <dodulana@gmail.com>
2026-02-19 15:00:50 +0100

    fix: publications queries

--- sanity/lib/getPublications.ts
+++ sanity/lib/getPublications.ts
@@ -1,6 +1,12 @@
 import { client } from "./client";
-import { publicationsQuery } from "./queries";
+import {
+  featuredPublicationQuery,
+  latestPublicationsQuery,
+} from "./queries";
 
 export async function getPublications() {
-  return client.fetch(publicationsQuery);
+  const featured = await client.fetch(featuredPublicationQuery);
+  const latest = await client.fetch(latestPublicationsQuery);
+
+  return { featured, latest };
 }
